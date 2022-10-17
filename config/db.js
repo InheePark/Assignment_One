d55ee942062161b5c54009f6295fcf148357868b
InheePark <59162676+InheePark@users.noreply.github.com>
2022-10-16 21:27:05 -0400

    connect to database

--- config/db.js
+++ config/db.js
@@ -1,1 +1,16 @@
-let atlasDB = "mongodb+srv://dbuser:dbuser@cluster005.asvptdk.mongodb.net/?retryWrites=true&w=majority";
+let atlasDB = "mongodb+srv://dbuser:dbuser@cluster005.asvptdk.mongodb.net/products?retryWrites=true&w=majority";
+
+let mongoose = require('mongoose');
+
+module.exports = function(){
+    
+    mongoose.connect(atlasDB);
+
+    let mongodb = mongoose.connection;
+    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+    mongodb.once('open', ()=>{
+        console.log('==== Connected to MongoDB ====');
+    });
+
+    return mongodb;
+}
